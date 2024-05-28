7c7b030c32a3a2149f3e1359ae8d79daa1e64867
Dorian Rakotonanahary <79524563+Drakoton@users.noreply.github.com>
2024-05-28 18:18:57 +0200

    Update main.js

--- algo/main.js
+++ algo/main.js
@@ -1,17 +1,14 @@
 function twoSum(array, target) {
-    const numIndices = {};  // Dictionnaire pour stocker les indices des éléments
+    const numIndices = {};
 
     for (let i = 0; i < array.length; i++) {
-        const complement = target - array[i];  // Calculer le complément nécessaire pour atteindre la cible
+        const complement = target - array[i];
 
         if (complement in numIndices) {
-            // Si le complément existe dans le dictionnaire, retourner les indices
             return [numIndices[complement], i];
         }
 
-        // Sinon, ajouter l'élément et son indice au dictionnaire
         numIndices[array[i]] = i;
     }
 
-    // Puisqu'il est supposé qu'il y a toujours une solution, on n'a pas besoin de retourner quoi que ce soit en dehors de la boucle
 }
